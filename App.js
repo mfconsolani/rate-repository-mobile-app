0f6e78dcafc6ba95e98031f886b99f73b0609ef9
mfconsolani <mfconsolani@gmail.com>
2021-10-18 22:00:37 +0200

    added initial files for repository rate app

---
+++ App.js
@@ -0,0 +1,11 @@
+// import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import Main from './src/components/Main';
+// import { StyleSheet, Text, View, Pressable, Alert } from 'react-native';
+
+const App = () => {
+  return <Main/>;
+};
+
+export default App;
+
---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Constants from 'expo-constants';
+import { Text, View, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+    flexShrink: 1
+  }
+});
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Rate Repository Application</Text>
+    </View>
+  );
+};
+
+export default Main;
